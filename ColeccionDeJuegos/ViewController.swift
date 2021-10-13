d4d8c779d7f18bf3b2d51cbf1d9724d344c77f02
Cesar Augusto Acjota Merma <cesar.acjota@tecsup.edu.pe>
2021-10-13 12:25:50 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Cesar Augusto Acjota Merma on 10/13/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
